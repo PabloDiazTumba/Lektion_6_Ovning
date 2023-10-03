f9acf49e8cca06194452a236d1c577656d0b1f22
PabloDiazTumba <pablodiaz147@gmail.com>
2023-10-03 21:00:11 +0200

    First commit

--- src/com/PabloDiaz/demo/Main.java
+++ src/com/PabloDiaz/demo/Main.java
@@ -5,7 +5,45 @@ package com.PabloDiaz.demo;
 public class Main {
     public static void main(String[] args) {
 
-        
+        /*
+        try {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        } finally {
+            System.out.println("The 'try catch' is finished.");
+        }
+        */
+
+
+        /*
+
+        int tia = 10;
+        int nolla = 0;
+
+        try {
+            int result = tia / nolla;
+            System.out.println("Resultatet är: " + result);
+        } catch (Exception e) {
+            System.out.println("Det går inte att dividera med noll");
+        }
+
+        for (;;) {
+            System.out.println("Krasch!");
+        }
+
+         */
+
+        int a = 10;
+        int b = 0;
+
+        if (((b == 0))) {
+            System.out.println("Krasch!");
+        } else {
+            int result = a / b;
+            System.out.println("Resultatet är: " + result);
+        }
 
     }
 }
